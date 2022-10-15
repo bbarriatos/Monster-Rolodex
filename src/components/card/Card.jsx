cc234b4a5be2d9b85385b68454379779e45d7f15
bbarriatos <bbarriatos@gmail.com>
2022-10-16 01:49:19 +0800

    Initial Commit

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./card-style.css";
+
+const Card = ({ monster: { name, email, id } }) => {
+  return (
+    <div key={id} className="card-container">
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
