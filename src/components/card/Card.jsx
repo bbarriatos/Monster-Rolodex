fe147bbad702ecf7e16bbb417e55672657df02c8
bbarriatos <bbarriatos@gmail.com>
2022-10-16 03:43:58 +0800

    test again

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,6 @@ import "./card-style.css";
 const Card = ({ monster: { name, email, id } }) => {
   return (
     <div key={id} className="card-container">
-      <h1>test update</h1>
       <img
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
         alt={`monster ${name}`}
